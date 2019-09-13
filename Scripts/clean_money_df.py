3c2c9210906c7b8b08e760d402a3b43d0bee6d66
Colin Maloney <colinmaloney@gmail.com>
2019-09-13 13:42:29 -0700

    remove scripts until fixed

--- Scripts/clean_money_df.py
+++
@@ -1,9 +0,0 @@
-def clean_money(value):
-    return (int(value.replace('$','').replace(',','')))
-
-
-final_df['Worldwide Box Office'] = final_df['Worldwide Box Office'].map(clean_money)
-final_df['Domestic Box Office'] = final_df['Domestic Box Office'].map(clean_money)
-final_df['International Box Office'] = final_df['International Box Office'].map(clean_money)
-
-final_df['DomesticShare'] = final_df['DomesticShare'].map(lambda x : float(x.replace('%','')))
